f43a7928afb17a941fec0b6f18e6e76752dc0ce2
johnkmeas <johnkmeas@gmail.com>
2017-01-17 02:37:39 +0000

    program takes arguments from commandline

--- download_avatars.js
+++ download_avatars.js
@@ -1,20 +1,23 @@
 var request = require('request');
-var fs = require('fs')
+var fs = require('fs');
+var argvLength = process.argv.slice(2);
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
+
+if(argvLength.length < 2 ){
+  console.log('Please give us a name and repository!');
+  throw err;
+}
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_USER = 'johnkmeas';
 var GITHUB_TOKEN = '981ab67211d4b78be4d0ef31f184366031a5d1da';
-var repoOwner = 'jquery';
-var repoName = 'jquery';
-
+// var repoOwner = 'jquery';
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
 
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
-  // var requestURL = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
   var requestOptions = {
     url: requestURL,
@@ -23,22 +26,36 @@ var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com
       "user-agent": 'johnkmeas'
     }
   }
-  // request.UserAgent = "myTestApp";
-  // console.log(requestOptions)
-  // console.log(requestOptions);
   request.get(requestOptions, cb);
-    // console.log(res, res.statusCode)
+}
 
+function downloadImageByURL(url, filePath) {
 
+  var dir = './avatars';
+
+  if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+  }
+
+  request.get(url)
+    .on('error', function (err) {
+      throw err;
+    }).on('response', function(response) {
+      console.log('Status: ',response.statusCode); // 200
+  }).pipe(fs.createWriteStream(dir + '/' + filePath));
 }
 
 getRepoContributors(repoOwner, repoName, function(err, result) {
-  console.log('Errors:', err);
 
-  var parse = JSON.parse(result.body)
-
+  var parse = JSON.parse(result.body);  //console.log(parse);
   parse.forEach(function(x){
-    console.log(x.avatar_url)
+     console.log(x.avatar_url);
+     console.log(x.login);
+     console.log('------');
+
+     //Calling function to download the images from the repository
+     downloadImageByURL(x.avatar_url, x.login +'.jpg' );
   })
 });
+
 
