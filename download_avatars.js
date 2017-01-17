ea9277db550075a3ae72e6d43fc7e613278e59db
johnkmeas <johnkmeas@gmail.com>
2017-01-17 00:36:21 +0000

    finally got the damn user agent to work

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,39 @@
 var request = require('request');
+var fs = require('fs')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+var GITHUB_USER = 'johnkmeas';
+var GITHUB_TOKEN = '981ab67211d4b78be4d0ef31f184366031a5d1da';
+var repoOwner = 'jquery';
+var repoName = 'jquery';
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  // var requestURL = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  var requestOptions = {
+    url: requestURL,
+    // bearer : GITHUB_TOKEN,
+    headers: {
+      "user-agent": 'johnkmeas'
+    }
+  }
+  // request.UserAgent = "myTestApp";
+  console.log(requestOptions)
+  // console.log(requestOptions);
+  request.get(requestOptions, cb);
+    // console.log(res, res.statusCode)
+
+
+}
+
+getRepoContributors(repoOwner, repoName, function(err, result) {
+  console.log('Errors:', err);
+  console.log('Result:', result.body);
+});
+
